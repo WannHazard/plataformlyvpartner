28008405895e8d787b6a4c37fa0afc2ab99beb9f
WannHazard <wann.rolerosyexportacion@gmail.com>
2025-12-04 17:10:52 -0300

    Fix CORS for www

--- server/index.js
+++ server/index.js
@@ -13,7 +13,8 @@ const PORT = process.env.PORT || 3000;
 const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:3000',
-  'https://platform.lyvpartner.com'
+  'https://platform.lyvpartner.com',
+  'https://www.platform.lyvpartner.com'
 ];
 
 app.use(cors({
